e8fe1bb09dc1a1c4382f804c1107977c6e89d466
ahenkel <anton.henke@stud.uni-heidelberg.de>
2025-02-13 15:03:07 +0100

    Generalize and clean up prediction accuracy scripts

--- prediction_accuracy_missing.py
+++ prediction_accuracy_missing.py
@@ -11,12 +11,13 @@ dataset_list = []
 emas = ['EMA_mood', 'EMA_disappointed', 'EMA_scared', 'EMA_worry', 'EMA_down', 'EMA_sad', 'EMA_confidence', 'EMA_stress', 'EMA_lonely', 'EMA_energetic', 'EMA_concentration', 'EMA_resilience', 'EMA_tired', 'EMA_satisfied', 'EMA_relaxed']
 emis = ['interactive1', 'interactive2', 'interactive3', 'interactive4', 'interactive5', 'interactive6', 'interactive7', 'interactive8']
 
-# Collect all CSV files with less than 50% missing rows from the specified subfolders
 prep_data_folder = "prep_data"
 subfolders = ["MRT1","MRT2","MRT3"]
 files = []
-missing_data_threshold = 0.7
-num_analysed_files = 0
+
+# Set the threshold for missing data and the number of valid rows
+missing_data_threshold = 1.0
+valid_rows_threshold = 50
 
 for subfolder in subfolders:
     folder_path = os.path.join(prep_data_folder, subfolder, "*.csv")
@@ -28,27 +29,36 @@ for subfolder in subfolders:
             dataset_list.append(data)
             files.append(file)
 
-mean_squared_error_list = []
-mean_absolute_error_per_var_list = []
-mean_absolute_error_list = []
-mean_real_absolute_difference_per_var_list = []
-mean_real_mae_list = []
-mean_real_mse_list = []
-high_mean_error_files = []
+mse_list = []
+baseline_mse_list = []
+mae_per_ema_list = []
+mae_list = []
 
+mac_per_ema_list = []
+mac_list = []
+msc_list = []
+
+#high_mean_error_files = []
 skip_files = {}
+num_analysed_files = 0
 for idx, dataset in enumerate(dataset_list):
     if files[idx] in skip_files:
         continue
-    num_analysed_files += 1
+
     X, U = dataset['X'], dataset['Inp']
     
     # Determine the split index for the training and testing data
     valid = ~np.isnan(X).any(axis=1)
     valid_rows = valid[:-1] & valid[1:] # valid rows are those where the predictor and target are both valid (no NaN values)
-    total = valid_rows.sum() # total number of useful rows
-    split_index = np.searchsorted(np.cumsum(valid_rows), total * 0.7) # searches for the index in (np.cumsum(pairs)) where the cumulative sum first exceeds or equals 70%
+    total = valid_rows.sum() # total number of valid rows
+    split_index = np.searchsorted(np.cumsum(valid_rows), total * 0.7) # searches for the index in (np.cumsum(pairs)) where the cumulative sum first exceeds or equals 70
     # Leads to lower standard deviation of the mean squared error and mean absolute error (compared to split_index = int(0.7 * len(X)))
+
+    # Skip if there are less than  valid row
+    if total < valid_rows_threshold:
+        continue
+
+    num_analysed_files += 1
     
     # Split data
     X_train, X_test = X[:split_index], X[split_index:]
@@ -82,91 +92,104 @@ for idx, dataset in enumerate(dataset_list):
 
     # Compute the mean squared error
     mse = np.mean((predicted_states - real_target_states)**2)
-    mean_squared_error_list.append(mse)
+    mse_list.append(mse)
+
+    # Compute the baseline mean squared error of the test data
+    target_mean_per_ema = np.mean(real_target_states, axis = 0)
+    target_mean_array = np.full_like(real_target_states, fill_value=target_mean_per_ema)
+    baseline_mse = np.mean((target_mean_array - real_target_states)**2)
+    baseline_mse_list.append(baseline_mse)
 
     # Compute the Mean Absolute Error (MAE)
     absolute_differences = np.abs(predicted_states - real_target_states)
     mean_absolute_errors = np.mean(absolute_differences, axis=0)
-    mean_absolute_error_per_var_list.append(mean_absolute_errors)
+    mae_per_ema_list.append(mean_absolute_errors)
 
     mae = np.mean(mean_absolute_errors)
-    mean_absolute_error_list.append(mae)
+    mae_list.append(mae)
 
-    # Compute the (real) mean absolute differences of the state per time step
+    # Compute the mean absolute change (MAC) of the state per time step
     real_absolute_differences = np.abs(real_predictor_states - real_target_states)
     mean_real_abs_differences = np.mean(real_absolute_differences, axis = 0)
-    mean_real_absolute_difference_per_var_list.append(mean_real_abs_differences)
+    mac_per_ema_list.append(mean_real_abs_differences)
 
-    real_mae = np.mean(mean_real_abs_differences)
-    mean_real_mae_list.append(real_mae)
+    mac = np.mean(mean_real_abs_differences)
+    mac_list.append(mac)
 
-    # Compute the (real) mean squared differences of the state per time step
-    squared_differences = np.square(real_absolute_differences)
-    mse_per_variable = np.mean(squared_differences, axis=0)
-    real_mse = np.mean(squared_differences)
-    mean_real_mse_list.append(real_mse)
+    # Compute the mean squared change (MSC) of the state per time step
+    real_squared_differences = np.square(real_absolute_differences)
+    msc = np.mean(real_squared_differences)
+    msc_list.append(msc)
 
     num_interventions_train = U_train.sum()
     num_interventions_test = U_test.sum()
 
     # Append to files_mean_error_greater_10 if mse > 10 to filter out bad datasets
-    if mse > 10:
-        high_mean_error_files.append(files[idx])
+    #if mse > 4:
+     #   high_mean_error_files.append(files[idx])
     
     # Print results
     print('-' * 40)
     print(f'{files[idx]}:')
-    print(f'Number of useful rows: {total}')
+    print(f'Number of valid rows: {total}')
     print(f'Split index (70%): {split_index}')
-    print(f'Mean Squared Error: {np.round(mse,2)} ({np.round(real_mse,2)})')
-    print(f'Mean Absolute Error: {np.round(mae,2)} ({np.round(real_mae,2)})')
-    # Print the MAE for each variable
-    df_mae_per_variable = pd.DataFrame({'MAE': np.round(mean_absolute_errors,2), 'real': np.round(mean_real_abs_differences,2)}, index=emas)
-    print(df_mae_per_variable)
+    print(f'Mean Squared Error: {np.round(mse,2)}')
+    print(f'Mean Squared Change: {np.round(msc,2)}')
+    print(f'Baseline Mean Squared Error: {np.round(baseline_mse,2)}')
+    print(f'Mean Absolute Error: {np.round(mae,2)}')
+    print(f'Mean Absolute Change: {np.round(mac,2)}')
+    #Print the MAE for each variable
+    #df_mae_per_variable = pd.DataFrame({'MAE': np.round(mean_absolute_errors,2), 'real': np.round(mean_real_abs_differences,2)}, index=emas)
+    #print(df_mae_per_variable)
     #print(f'Number of interventions in training data: {num_interventions_train}')
     #print(f'Number of interventions in testing data: {num_interventions_test}')
     
-mean_squared_errors = np.array(mean_squared_error_list)
-max_mean_squared_error = np.max(mean_squared_errors)
-min_mean_squared_error = np.min(mean_squared_errors)
-mean_mean_squared_error = np.mean(mean_squared_errors)
-std_mean_squared_error = np.std(mean_squared_errors)
+mse_array = np.array(mse_list)
+max_mse = np.max(mse_array)
+min_mse = np.min(mse_array)
+mean_mse = np.mean(mse_array)
+std_mse = np.std(mse_array)
 
-mean_absolute_errors = np.array(mean_absolute_error_list)
-max_mean_absolute_error = np.max(mean_absolute_errors)
-min_mean_absolute_error = np.min(mean_absolute_errors)
-mean_mean_absolute_error = np.mean(mean_absolute_errors)
-std_mean_absolute_error = np.std(mean_absolute_errors)
+baseline_mse_array = np.array(baseline_mse_list)
+mean_baseline_mse = np.mean(baseline_mse_array)
 
-mean_absolute_errors_per_var = np.array(mean_absolute_error_per_var_list)
-mean_mean_abs_error_per_var = np.mean(mean_absolute_errors_per_var, axis=0)
+mae_array = np.array(mae_list)
+max_mae = np.max(mae_array)
+min_mae = np.min(mae_array)
+mean_mae = np.mean(mae_array)
+std_mae = np.std(mae_array)
 
-mean_differences_per_var = np.array(mean_real_absolute_difference_per_var_list)
-mean_mean_differences_per_var = np.mean(mean_differences_per_var, axis=0)
+mae_per_ema_array= np.array(mae_per_ema_list)
+mean_mae_per_ema = np.mean(mae_per_ema_array, axis=0)
 
-mean_real_mses = np.array(mean_real_mse_list)
-mean_mean_mses = np.mean(mean_real_mses)
+mac_per_ema_array = np.array(mac_per_ema_list)
+mean_mac_per_ema = np.mean(mac_per_ema_array, axis=0)
+mean_mac = np.mean(mean_mac_per_ema)
+
+msc_array = np.array(msc_list)
+mean_msc = np.mean(msc_array)
 
 df_mean_mae = pd.DataFrame({
-    'MAE': np.round(mean_mean_abs_error_per_var,2),
-    'real': np.round(mean_mean_differences_per_var,2)
+    'MAE': np.round(mean_mae_per_ema,2),
+    'MAC': np.round(mean_mac_per_ema,2)
 }, index = emas)
 
 print()
 print('#' * 40)
 print()
 print(f'Number of datasets analysed: {num_analysed_files}')
-print(f'Datasets with Mean Squared Error > 10: {high_mean_error_files}') # to filter out bad datasets
+#print(f'Datasets with Mean Squared Error > 4: {high_mean_error_files}') # to filter out bad datasets
 print()
 #print(f'Max Mean Squared Error: {max_mean_squared_error}')
 #print(f'Min Mean Squared Error: {min_mean_squared_error}')
-print(f'Mean Mean Squared Error: {np.round(mean_mean_squared_error,2)} ({np.round(mean_mean_mses,2)})')
-print(f'Std Mean Squared Error: {np.round(std_mean_squared_error,2)}')
+print(f'Mean Mean Squared Error: {np.round(mean_mse,2)} ({np.round(std_mse,2)})')
+print(f'Mean Mean Squared Change: {np.round(mean_msc,2)}')
+print(f'Mean Baseline Mean Squared Error: {np.round(mean_baseline_mse,2)}')
 print()
 #print(f'Max Mean Absolute Error: {max_mean_absolute_error}')
 #print(f'Min Mean Absolute Error: {min_mean_absolute_error}')
-print(f'Mean Mean Absolute Error: {np.round(mean_mean_absolute_error,2)} ({np.round(np.mean(mean_mean_differences_per_var),2)})')
-print(f'Std Mean Absolute Error: {np.round(std_mean_absolute_error,2)}')
+print(f'Mean Mean Absolute Error: {np.round(mean_mae,2)} ({np.round(std_mae,2)})')
+print(f'Mean Mean Absolute Change: {np.round(mean_mac,2)}')
 print()
-print(f'Mean Absolute Error (model prediction) vs real mean differences per variable:')
+print(f'MAE and MAC per EMA:')
 print(df_mean_mae)
