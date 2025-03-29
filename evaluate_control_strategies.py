Modify script to not accumulate steps, also fix issues (correct control input matrix for rnn, correct difference computation, fixed online scenario...

evaluate_control_strategies.py
@@ -22,35 +22,26 @@ emis = ['interactive1', 'interactive2', 'interactive3', 'interactive4', 'interac
 data_folder = "data"
 subfolders = ["MRT1/processed_csv_no_con","MRT2/processed_csv_no_con","MRT3/processed_csv_no_con"]
 
-num_rows_threshold = 50 # One file is excluded
-
+online = False
 n_steps = [1,2,3,4,5,6,7,8,9,10,11,12]
 brute_force_time_horizon = 5
 rho = 1
 
-dataset_list, files = utils.load_dataset(data_folder, subfolders, emas, emis, centered=False, exclude_constant_columns=True)
+dataset_list_lds, files = utils.load_dataset(data_folder, subfolders, emas, emis, centered=True, exclude_constant_columns=True)
 dataset_list_rnn, _ = utils.load_dataset(data_folder, subfolders, emas, emis, centered=False, exclude_constant_columns=False)
+
+def get_valid_ratio(data):
+    # Valid rows are rows without nan value where also the next row has no nan value
+    valid = ~np.isnan(data).any(axis=1)
+    valid_rows = valid[:-1] & valid[1:]
+    total = valid_rows.sum()
+    return total/len(data)
 
 def locf(X_train): 
     df_helper_locf = pd.DataFrame(X_train).copy()
     df_helper_locf.ffill(inplace=True)
     X_train_locf = df_helper_locf.to_numpy()
     return X_train_locf
-
-def compute_real_future_mean(data, index, n_steps):
-    future_steps = data[index + 1 : index + n_steps + 1]
-    if index + n_steps >= data.shape[0] or np.isnan(future_steps).all():
-        return False
-    future_mean = np.nanmean(future_steps, axis=0)
-    return future_mean
-
-def compute_predictions_mean(predictions, constant_columns):
-    if constant_columns:
-        # Exclude constant columns
-        filtered_predictions = np.delete(predictions, constant_columns, axis=1)
-        return np.mean(filtered_predictions, axis=0)
-    else:
-        return np.mean(predictions, axis=0)
 
 def predict_n_steps_lds(locf_X, U, index, control_input, n_steps):
     A, B, lmbda = utils.stable_ridge_regression(locf_X, U)
@@ -73,7 +64,7 @@ def get_model_paths(participant_nr, folder_path):
             model_paths_dict[timestep] = os.path.join(folder_path, filename)
     return model_paths_dict
 
-def find_model_path(file):
+def find_model_path(file, index=None):
     # Get MRT number
     match = re.search(r"MRT(\d)", file)
     mrt_nr = int(match.group(1))
@@ -85,18 +76,29 @@ def find_model_path(file):
     model_paths_dict = get_model_paths(participant_nr, model_path)
     if not model_paths_dict:
         return False
-    # Choose the last trained model
-    max_key = max(model_paths_dict.keys())
-    last_model_path = model_paths_dict[max_key]
-    return last_model_path
+    if index is not None:
+        # Online scenario: Choose model that is trained up to index
+        valid_keys = [k for k in model_paths_dict.keys() if k <= index]
+        if not valid_keys:
+            return False
+        key_online = max(valid_keys)  # Get the closest model up to `index`
+        model_path_scenario = model_paths_dict[key_online] 
+    else:
+        # Offline scenario: Choose the last trained model
+        max_key = max(model_paths_dict.keys())
+        model_path_scenario = model_paths_dict[max_key] 
+    return model_path_scenario
 
-def predict_n_steps_rnn(locf_X, U, index, control_input, n_steps, model_path):
-    locf_X_tensor = tc.from_numpy(locf_X).to(dtype=tc.float32)
-    U_tensor = tc.from_numpy(U).to(dtype=tc.float32)
+def predict_n_step_rnn(index_row, control_input, n_step, model_path, constant_columns):
+    index_row_tensor = tc.from_numpy(index_row).to(dtype=tc.float32)
+
+    # U_strategy is a 2D tensor where the first row is the control input and the rest is filled with zeros
     control_input_tensor = tc.from_numpy(control_input).to(dtype=tc.float32)
-    U_strategy = U_tensor.clone()
-    U_strategy[index] = control_input_tensor
+    U_strategy = tc.zeros((n_step, len(emis)), dtype=tc.float32)
+    U_strategy[0] = control_input_tensor
+
     predictions_overall = []
+    
     for i in range(10):
         model_nr = str(i+1).zfill(3)
         model_path_specific = os.path.join(model_path, model_nr)
@@ -105,13 +107,17 @@ def predict_n_steps_rnn(locf_X, U, index, control_input, n_steps, model_path):
         except AssertionError as e:
             print(f"Error: {e}. No model found at {model_path_specific}. Exiting function.")
             return []
-        predictions = model.generate_free_trajectory(locf_X_tensor[index], n_steps, inputs = U_strategy[index:index+n_steps], prewarm_data=None, prewarm_inputs=None)
-        predictions_overall.append(predictions)
+        predictions = model.generate_free_trajectory(index_row_tensor, n_step, inputs = U_strategy, prewarm_data=None, prewarm_inputs=None)
+
+        predictions_overall.append(predictions[n_step-1])
+
     predictions_stacked = tc.stack(predictions_overall)
-    # Compute the mean across across the 10 models  
-    predictions_mean = tc.mean(predictions_stacked, dim=0)
-    predictions_mean_numpy = predictions_mean.numpy()
-    return predictions_mean_numpy
+    # Compute the mean across across the 10 models
+    predictions_mean = predictions_stacked.mean(dim=0)
+
+    predictions_numpy = predictions_mean.numpy()
+    predictions_numpy[constant_columns] = np.nan # Filter out predictions for constant columns
+    return predictions_numpy
 
 def mean_and_standard_error(data):
     data = np.array(data)
@@ -125,25 +131,28 @@ filenames = []
 overall_wellbeing_change_opt_ctrl = {step: [] for step in n_steps}
 overall_wellbeing_change_brute_force = {step: [] for step in n_steps}
 overall_wellbeing_change_max_ac = {step: [] for step in n_steps}
-
+overall_wellbeing_change_no_emi = {step: [] for step in n_steps}
 
 count_nan = 0
 
-for idx, (dataset, dataset_rnn) in enumerate(zip(dataset_list, dataset_list_rnn)):
+for idx, (dataset, dataset_rnn) in enumerate(zip(dataset_list_lds, dataset_list_rnn)):
+    print(files[idx])
+
+    # Model path for offline scenario, when online scenario 'model_path_rnn' gets overwritten
     model_path_rnn = find_model_path(files[idx])
 
-    X, U = dataset['X'], dataset['Inp']
-    X_rnn = dataset_rnn['X']
-    
-    if len(X) < num_rows_threshold:
+    if model_path_rnn == False:
+        print("No model found\n")
         continue
 
-    
-    print(files[idx])
-    if model_path_rnn == False:
-        print('Failed to get model path')
-        print()
-        continue
+    # Exclude constant columns: used to compute the control strategies (infer A and B on X, U)
+    X, U = dataset['X'], dataset['Inp']
+    # Keep all columns because RNN needs shape (.., 15) to make predictions
+    X_rnn = dataset_rnn['X']
+
+    #valid_ratio = get_valid_ratio(X)
+    #if valid_ratio < 0.8:
+    #    continue      
     
     n_items = X.shape[1]
     n_inputs = U.shape[1]
@@ -152,70 +161,97 @@ for idx, (dataset, dataset_rnn) in enumerate(zip(dataset_list, dataset_list_rnn)
 
     input_rows_indices = np.where(~np.all(U == 0, axis=1))[0].tolist()
 
-    real_future_means = {step: [] for step in n_steps}
-    predicted_means_opt_ctrl = {step: [] for step in n_steps}
-    predicted_means_brute_force = {step: [] for step in n_steps}
-    predicted_means_max_ac = {step: [] for step in n_steps}
+    wellbeing_differences_opt_ctrl = {step: [] for step in n_steps}
+    wellbeing_differences_brute_force = {step: [] for step in n_steps}
+    wellbeing_differences_max_ac = {step: [] for step in n_steps}
+    wellbeing_differences_no_emi = {step: [] for step in n_steps}
 
+    locf_X = locf(X) # In X constant columns are excluded
+    locf_X_rnn = locf(X_rnn)
+
+    # To exclude constant columns in the predictions
+    constant_columns = [i for i in range(locf_X_rnn.shape[1]) if np.unique(locf_X_rnn[:, i]).size == 1]
+
+    if not online:
+        input_opt_ctrl_offline = strats.optimal_control_strategy(locf_X, U, target_state, admissible_inputs, rho, online)
+        input_brute_force_offline = strats.brute_force_strategy(locf_X, U, target_state, admissible_inputs, brute_force_time_horizon, rho, online)
+        input_max_ac_offline = strats.max_ac_strategy(locf_X, U, admissible_inputs, online)
 
     for index in input_rows_indices:
-        if index < 20:
-            continue
 
-        locf_X = locf(X)
-        locf_X_rnn = locf(X_rnn)
+        if online:
+            if index < 80:
+                # There are no models trained to now < 80
+                continue
+            locf_X_online = locf_X[:index, :]
+            U_online = U[:index, :]
 
-        constant_columns = [i for i in range(locf_X_rnn.shape[1]) if np.unique(locf_X_rnn[:, i]).size == 1]
+            input_opt_ctrl = strats.optimal_control_strategy(locf_X_online, U_online, target_state, admissible_inputs, rho, online)
+            input_brute_force = strats.brute_force_strategy(locf_X_online, U_online, target_state, admissible_inputs, brute_force_time_horizon, rho, online)
+            input_max_ac = strats.max_ac_strategy(locf_X_online, U_online, admissible_inputs, online)
 
-        input_opt_ctrl = strats.optimal_control_strategy(locf_X, U, target_state, admissible_inputs, rho, online=True)
-        input_brute_force = strats.brute_force_strategy(locf_X, U, target_state, admissible_inputs, brute_force_time_horizon, rho, online=True)
-        input_max_ac = strats.max_ac_strategy(locf_X, U, admissible_inputs, online=True)
+            model_path_rnn = find_model_path(files[idx], index)
+            if model_path_rnn == False:
+                # Often there are no models trained to now <= index
+                continue
+
+        else:
+            # The offline control strategies return 2d arrays the same shape as X
+            input_opt_ctrl = input_opt_ctrl_offline[index]
+            input_brute_force = input_brute_force_offline[index]
+            input_max_ac = input_max_ac_offline[index]
 
         for step in n_steps:
-            real_future_mean = compute_real_future_mean(X_rnn, index, step)
-            if real_future_mean is False:
+            if index + step >= len(X_rnn):
+                break
+
+            index_row = X_rnn[index]
+            if np.isnan(index_row).all():
+                break
+            
+            real_future_step = X_rnn[index + step] # Assume the intervention is done after the emas are answered
+            if np.isnan(real_future_step).all():
                 continue
-            real_future_means[step].append(real_future_mean)
 
-            predictions_opt_ctrl = predict_n_steps_rnn(locf_X_rnn, U, index, input_opt_ctrl, step, model_path_rnn)
-            predicted_means_opt_ctrl[step].append(compute_predictions_mean(predictions_opt_ctrl, constant_columns))
+            prediction_opt_ctrl = predict_n_step_rnn(index_row, input_opt_ctrl, step, model_path_rnn, constant_columns)
+            #print(f'step = {step}, index = {index}')
+            #print(index_row)
+            #print(prediction_opt_ctrl)
+            #print(real_future_step)
+            #print()
+            wellbeing_differences_opt_ctrl[step].append(prediction_opt_ctrl - real_future_step)
 
-            predictions_brute_force = predict_n_steps_rnn(locf_X_rnn, U, index, input_brute_force, step, model_path_rnn)
-            predicted_means_brute_force[step].append(compute_predictions_mean(predictions_brute_force, constant_columns))
+            prediction_brute_force = predict_n_step_rnn(index_row, input_brute_force, step, model_path_rnn, constant_columns)
+            wellbeing_differences_brute_force[step].append(np.mean(prediction_brute_force - real_future_step))
 
-            predictions_max_ac = predict_n_steps_rnn(locf_X_rnn, U, index, input_max_ac, step, model_path_rnn)
-            predicted_means_max_ac[step].append(compute_predictions_mean(predictions_max_ac, constant_columns))
+            prediction_max_ac = predict_n_step_rnn(index_row, input_max_ac, step, model_path_rnn, constant_columns)
+            wellbeing_differences_max_ac[step].append(np.mean(prediction_max_ac - real_future_step))
+
+            prediction_no_emi = predict_n_step_rnn(index_row, np.zeros(n_inputs), step, model_path_rnn, constant_columns)
+            wellbeing_differences_no_emi[step].append(np.mean(prediction_no_emi - real_future_step))
 
     for step in n_steps:
-        if not real_future_means[step]:  # Skip if no data collected
-            continue
         
-        real_mean = np.mean(real_future_means[step])
-        predicted_opt_ctrl_mean = np.mean(predicted_means_opt_ctrl[step])
-        predicted_brute_force_mean = np.mean(predicted_means_brute_force[step])
-        predicted_max_ac_mean = np.mean(predicted_means_max_ac[step])
-
-        difference_wellbeing_opt_ctrl = predicted_opt_ctrl_mean - real_mean
-        difference_wellbeing_brute_force = predicted_brute_force_mean - real_mean
-        difference_wellbeing_max_ac = predicted_max_ac_mean - real_mean
+        wellbeing_differences_opt_ctrl_mean = np.mean(wellbeing_differences_opt_ctrl[step])
+        wellbeing_differences_brute_force_mean = np.mean(wellbeing_differences_brute_force[step])
+        wellbeing_differences_max_ac_mean = np.mean(wellbeing_differences_max_ac[step])
+        wellbeing_differences_no_emi_mean = np.mean(wellbeing_differences_no_emi[step])
 
         print(f'Prediction Length (step): {step}')
         print(f'constant columns: {constant_columns}')
-        print(f'opt_ctrl difference: {difference_wellbeing_opt_ctrl}')
-        print(f'brute_force difference: {difference_wellbeing_brute_force}')
-        print(f'max_ac difference: {difference_wellbeing_max_ac}')
+        print(f'opt_ctrl difference: {wellbeing_differences_opt_ctrl_mean}')
+        print(f'brute_force difference: {wellbeing_differences_brute_force_mean}')
+        print(f'max_ac difference: {wellbeing_differences_max_ac_mean}')
+        print(f'no emi difference: {wellbeing_differences_no_emi_mean}')
         print()
-
-        if np.isnan(difference_wellbeing_opt_ctrl):
-            count_nan += 1
-            continue
 
         if step == 1:  # Store filenames only once per dataset
             filenames.append(files[idx])
 
-        overall_wellbeing_change_opt_ctrl[step].append(difference_wellbeing_opt_ctrl)
-        overall_wellbeing_change_brute_force[step].append(difference_wellbeing_brute_force)
-        overall_wellbeing_change_max_ac[step].append(difference_wellbeing_max_ac)
+        overall_wellbeing_change_opt_ctrl[step].append(wellbeing_differences_opt_ctrl_mean)
+        overall_wellbeing_change_brute_force[step].append(wellbeing_differences_brute_force_mean)
+        overall_wellbeing_change_max_ac[step].append(wellbeing_differences_max_ac_mean)
+        overall_wellbeing_change_no_emi[step].append(wellbeing_differences_no_emi_mean)
     print(40*"-")
 
 extracted_filenames = []
@@ -225,7 +261,6 @@ for file in filenames:
     extracted_part = f"{mrt_number}-{file_name}"
     extracted_filenames.append(extracted_part)
 
-print(f'nan count = {count_nan}')
 
 # Convert results into DataFrame format
 data = {"file": extracted_filenames}
@@ -233,18 +268,21 @@ for step in n_steps:
     data[f"optimal control (n={step})"] = overall_wellbeing_change_opt_ctrl.get(step, [])
     data[f"brute force (n={step})"] = overall_wellbeing_change_brute_force.get(step, [])
     data[f"max ac (n={step})"] = overall_wellbeing_change_max_ac.get(step, [])
+    data[f"no emi (n={step})"] = overall_wellbeing_change_no_emi.get(step, [])
 
 df_results = pd.DataFrame(data)
-filepath1 = os.path.join("results_control_strategies", "results_rnn_steps_mean.csv")
+filepath1 = os.path.join("results_control_strategies", "results_rnn_offline_step.csv")
 df_results.to_csv(filepath1, index=False)
 print(f"Results saved to {filepath1}")
 
 # Questions to this approach:
-# Should you use the latest model (greatest now) for the predictions?
-# Should you include interventions in nan-rows?
+# Should you use the latest model (greatest now) for the predictions -> Yes?
+# Is the intervention done after the emas are answered in a time step
+# Should you include interventions in nan-rows? I exclude them because this row serves as input for the rnn to make the predictions
 
-# Should you compute the difference of the means of the next n_steps or only for the steps that arent nan in the real data (second is better because you dont make assumption that they would answer mean)
-# Should every file contribute equally to the mean wellbeing difference per strategy?
+# Should every participant contribute equally to the mean wellbeing difference per strategy?
 #  -> should the mean be taken over the wellbeing difference per intervention rather than participant?
 
-# Maybe test if the model has the tendency to predict lower values (how?)
+# Maybe test if the model has the tendency to predict lower values (how?) -> by comparing it with the prediction for no-emi
+
+# Online scenario: should U be updated with the control input to compute further control inputs